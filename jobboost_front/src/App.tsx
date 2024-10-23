4a21faaedc4a41a6181e446cbd4422e9b95b261b
JulesNyom <julesnyom@gmail.com>
2024-10-23 14:36:21 +0200

    Added the core of the landing page.

--- jobboost_front/src/App.tsx
+++ jobboost_front/src/App.tsx
@@ -1,11 +1,19 @@
 import './App.css'
+import Features from './components/shared/Features'
+import Footer from './components/shared/Footer'
 import  Header  from './components/shared/Header'
+import Hero from './components/shared/Hero'
+import How from './components/shared/How'
 
 function App() {
 
   return (
     <>
     <Header/>
+    <Hero/>
+    <Features/>
+    <How/>
+    <Footer/>
     </>
   )
 }
